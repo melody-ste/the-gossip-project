43d81a33ef3431ef24875951bfa50463fde5522b
melody-ste <melody_stephan@icloud.com>
2025-06-09 14:16:12 +0200

    page home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  root "static_pages#home"
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
