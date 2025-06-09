59af8f367d490a75d366a640e22cddc2a9dec1cc
Paarroo <code.chill717@passinbox.com>
2025-06-09 15:10:51 +0200

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,9 @@
 Rails.application.routes.draw do
-  get "users/show"
-  get "gossips/index"
-  get "gossips/show"
-  get "static_pages/team"
-  get "static_pages/contact"
-  get "static_pages/welcome"
 
   root "static_pages#home"
   get '/team', to: 'static_pages#team'
   get '/contact', to: 'static_pages#contact'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+ get '/welcome/:first_name', to: 'static_pages#welcome'
+get '/users/:id', to : 'users#show'
 end
