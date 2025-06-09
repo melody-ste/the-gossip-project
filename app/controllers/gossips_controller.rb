8654f6ff7dfe99313718e45a65bf18ecf3d3deff
Paarroo <code.chill717@passinbox.com>
2025-06-09 18:07:51 +0200

    controllers +viewWelcome

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -1,7 +1,9 @@
 class GossipsController < ApplicationController
   def index
+    @gossips = Gossip.includes(:user).all
   end
 
   def show
+    @gossip = Gossip.includes(user: :city).find(params[:id])
   end
 end
