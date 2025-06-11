a3370dca4efeede9f896e571cdf3fec0993835f7
melody-ste <melody_stephan@icloud.com>
2025-06-11 14:09:21 +0200

    ajout de supprimer le gossip

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -3,7 +3,7 @@ class Gossip < ApplicationRecord
   validates :content, presence: true
 
   belongs_to :user
-  has_many :gossip_tags
+  has_many :gossip_tags, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :tags, through: :gossip_tags
 end
